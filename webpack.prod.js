28c24c4e273e8f5eb9e33368a7be53ed0a72b4db
pikou1995 <434708082@qq.com>
2020-10-23 13:32:00 +0800

    pwa

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,13 +3,15 @@ const common = require('./webpack.common.js')
 const TerserPlugin = require('terser-webpack-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 const path = require('path')
+const { PUBLIC_PATH = '/' } = process.env
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: '[name].[hash].js',
-    publicPath: '/price/',
+    publicPath: PUBLIC_PATH,
     path: path.join(__dirname, './dist'),
   },
   optimization: {
@@ -25,6 +27,12 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css',
       ignoreOrder: true,
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
     }),
   ],
 })
