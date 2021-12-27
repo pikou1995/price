376fea6fb08d7de8126ef5c48823179eec195f89
pikou1995 <434708082@qq.com>
2021-12-27 21:41:22 +0800

    fix: 修复累加浮点数问题

--- resources/utils.ts
+++ resources/utils.ts
@@ -20,3 +20,22 @@ export function getInitialState() {
     }
   }
 }
+
+/**
+ * 解决浮点数相加问题
+ */
+export function accAdd(arg1: number, arg2: number) {
+  let r1, r2
+  try {
+    r1 = arg1.toString().split('.')[1].length
+  } catch (e) {
+    r1 = 0
+  }
+  try {
+    r2 = arg2.toString().split('.')[1].length
+  } catch (e) {
+    r2 = 0
+  }
+  const m = Math.pow(10, Math.max(r1, r2))
+  return (arg1 * m + arg2 * m) / m
+}
